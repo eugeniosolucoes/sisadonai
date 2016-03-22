bbda000069cbde3f1bf5e7a0f32866719dd4134c
eugenio <eugenio@vmcpo-2021>
2016-03-21 22:12:11 -0300

    Inicio do desenvolvimento de "Processar Arquivo de Retorno".

---
+++ src/main/java/br/com/eugeniosolucoes/service/impl/ArquivoDeRetornoServiceImpl.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.eugeniosolucoes.service.impl;
+
+import br.com.eugeniosolucoes.excecoes.CepException;
+import br.com.eugeniosolucoes.excecoes.TamanhoLinhaException;
+import br.com.eugeniosolucoes.service.ArquivoDeRetornoService;
+import br.com.eugeniosolucoes.util.TratadorArquivoRemessa;
+import br.com.eugeniosolucoes.view.model.DadosBoletoPagoModel;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author eugenio
+ */
+public class ArquivoDeRetornoServiceImpl implements ArquivoDeRetornoService {
+
+    private static final Logger LOG = Logger.getLogger( ArquivoDeRetornoServiceImpl.class.getName() );
+
+    @Override
+    public List<DadosBoletoPagoModel> lerArquivoDeRetorno( File file ) {
+        StringBuilder arquivo = new StringBuilder();
+        StringBuilder erro = new StringBuilder();
+        int cont = 0;
+        try ( Scanner scanner = new Scanner( file ) ) {
+            while (scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                cont++;
+            }
+        } catch ( FileNotFoundException ex ) {
+            LOG.log( Level.SEVERE, ex.getMessage(), ex );
+            throw new IllegalStateException( "Arquivo não encontrado!" );
+        }
+        return Collections.EMPTY_LIST;
+    }
+
+    @Override
+    public void processarBaixaDeBoleto( DadosBoletoPagoModel boletoPagoModels ) {
+        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void processarBaixaDeBoleto( List<DadosBoletoPagoModel> boletoPagoModels ) {
+        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
