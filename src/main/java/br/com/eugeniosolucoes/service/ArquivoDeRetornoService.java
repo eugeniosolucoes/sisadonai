116b17fa0333acf0c5383bd8bf0d297b733ecea1
eugenio <eugenio@vmcpo-2021>
2016-03-21 21:59:03 -0300

    Inicio do desenvolvimento de "Processar Arquivo de Retorno".

---
+++ src/main/java/br/com/eugeniosolucoes/service/ArquivoDeRetornoService.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.eugeniosolucoes.service;
+
+import br.com.eugeniosolucoes.view.model.DadosBoletoPagoModel;
+import java.io.File;
+import java.util.List;
+
+/**
+ *
+ * @author eugenio
+ */
+public interface ArquivoDeRetornoService {
+
+    /**
+     * Ler arquivo de Retorno e criar uma lista de boletos pagos.
+     * 
+     * @param file Arquivo de Retorno.
+     * @return Lista de boletos pagos.
+     */
+    List<DadosBoletoPagoModel> lerArquivoDeRetorno( File file );
+
+    /**
+     * Processar a baixa de um boleto pago no sistema.
+     * 
+     * @param boletoPagoModels boletos pago.
+     */
+    void processarBaixaDeBoleto( DadosBoletoPagoModel  boletoPagoModels );
+    
+    /**
+     * Processar a lista de boletos pagos efetuando a baixa no sistema.
+     * 
+     * @param boletoPagoModels Lista de boletos pagos.
+     */
+    void processarBaixaDeBoleto( List<DadosBoletoPagoModel>  boletoPagoModels );
+
+}
---
+++ src/main/java/br/com/eugeniosolucoes/view/model/DadosBoletoPagoModel.java
@@ -0,0 +1,202 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.eugeniosolucoes.view.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author eugenio
+ */
+public class DadosBoletoPagoModel {
+
+    private String cpf;
+
+    private String matricula;
+
+    private String aluno;
+
+    private String nossoNumero;
+
+    private Double valor;
+
+    private Date vencimento;
+
+    private String situacaoMensalidade;
+
+    private String numeroMensalidade;
+
+    private String quantidadeMensalidade;
+
+    private Double percentualMulta;
+
+    private Double percentualJuros;
+
+
+    public DadosBoletoPagoModel() {
+    }
+
+    public DadosBoletoPagoModel( String cpf, String matricula, String aluno, 
+            String nossoNumero, Double valor, Date vencimento,
+            String situacaoMensalidade, String numeroMensalidade, String quantidadeMensalidade,
+            Double percentualMulta, Double percentualJuros ) {
+        this.cpf = cpf;
+        this.matricula = matricula;
+        this.aluno = aluno;
+        this.nossoNumero = nossoNumero;
+        this.valor = valor;
+        this.vencimento = vencimento;
+        this.situacaoMensalidade = situacaoMensalidade;
+        this.numeroMensalidade = numeroMensalidade;
+        this.quantidadeMensalidade = quantidadeMensalidade;
+        this.percentualMulta = percentualMulta;
+        this.percentualJuros = percentualJuros;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula( String matricula ) {
+        this.matricula = matricula;
+    }
+
+    public String getAluno() {
+        return aluno;
+    }
+
+    public void setAluno( String aluno ) {
+        this.aluno = aluno;
+    }
+
+    public String getNossoNumero() {
+        return nossoNumero;
+    }
+
+    public void setNossoNumero( String nossoNumero ) {
+        this.nossoNumero = nossoNumero;
+    }
+
+    public String getNumeroDocumento() {
+        return String.format( "%s-%s", this.matricula, this.numeroMensalidade );
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor( Double valor ) {
+        this.valor = valor;
+    }
+
+    public Date getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento( Date vencimento ) {
+        this.vencimento = vencimento;
+    }
+
+    public String getSituacaoMensalidade() {
+        return situacaoMensalidade;
+    }
+
+    public void setSituacaoMensalidade( String situacaoMensalidade ) {
+        this.situacaoMensalidade = situacaoMensalidade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getCpfFormatado() {
+        StringBuilder sb = new StringBuilder( cpf );
+        sb.insert( 3, "." );
+        sb.insert( 7, "." );
+        sb.insert( 11, "-" );
+        return sb.toString();
+    }
+
+    public void setCpf( String cpf ) {
+        this.cpf = cpf;
+    }
+
+    public String getNumeroMensalidade() {
+        return numeroMensalidade;
+    }
+
+    public void setNumeroMensalidade( String numeroMensalidade ) {
+        this.numeroMensalidade = numeroMensalidade;
+    }
+
+    public String getQuantidadeMensalidade() {
+        return quantidadeMensalidade;
+    }
+
+    public void setQuantidadeMensalidade( String quantidadeMensalidade ) {
+        this.quantidadeMensalidade = quantidadeMensalidade;
+    }
+
+    public Double getPercentualMulta() {
+        return percentualMulta;
+    }
+
+    public void setPercentualMulta( Double percentualMulta ) {
+        this.percentualMulta = percentualMulta;
+    }
+
+    public Double getPercentualJuros() {
+        return percentualJuros;
+    }
+
+    public void setPercentualJuros( Double percentualJuros ) {
+        this.percentualJuros = percentualJuros;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode( this.cpf );
+        hash = 29 * hash + Objects.hashCode( this.matricula );
+        hash = 29 * hash + Objects.hashCode( this.numeroMensalidade );
+        return hash;
+    }
+
+    @Override
+    public boolean equals( Object obj ) {
+        if ( this == obj ) {
+            return true;
+        }
+        if ( obj == null ) {
+            return false;
+        }
+        if ( getClass() != obj.getClass() ) {
+            return false;
+        }
+        final DadosBoletoPagoModel other = (DadosBoletoPagoModel) obj;
+        if ( !Objects.equals( this.cpf, other.cpf ) ) {
+            return false;
+        }
+        if ( !Objects.equals( this.matricula, other.matricula ) ) {
+            return false;
+        }
+        return Objects.equals( this.numeroMensalidade, other.numeroMensalidade );
+    }
+
+    
+
+    public boolean isBoletoValido() {
+        return Long.valueOf( this.nossoNumero ) != 0L;
+    }
+
+    @Override
+    public String toString() {
+        return "DadosBoletoPagoModel{" + "cpf=" + cpf + ", matricula=" + matricula + ", aluno=" + aluno + ", numeroMensalidade=" + numeroMensalidade + '}';
+    }
+
+    
+}
